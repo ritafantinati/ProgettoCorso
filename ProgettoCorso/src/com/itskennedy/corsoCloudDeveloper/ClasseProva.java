48108061a867bcb084e4c71cc5261368e5445fb0
ITSFantinatiR <ITSFantinatiR@itscor207389.itsacademy.corvallisholding.it>
2021-03-03 10:12:37 +0100

    ultimo commit

--- ProgettoCorso/src/com/itskennedy/corsoCloudDeveloper/ClasseProva.java
+++ ProgettoCorso/src/com/itskennedy/corsoCloudDeveloper/ClasseProva.java
@@ -2,6 +2,6 @@ package com.itskennedy.corsoCloudDeveloper;
 
 public class ClasseProva {
 	public static void main(String[] args) {
-		System.out.println("hey");
+		String s = new String("");
 	}
 }
